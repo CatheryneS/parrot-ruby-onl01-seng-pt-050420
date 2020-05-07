97e1068a9f14fa6b4cdf77224cc3887e7adec6dd
Catheryne Small <catheryn.small@gmail.com>
2020-05-07 01:24:37 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,6 +1,7 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
-def parrot = "Pretty bird!"
+def parrot
+  parrot = "Pretty bird!"
   puts "Pretty bird!"
   "Pretty bird!"
 end
