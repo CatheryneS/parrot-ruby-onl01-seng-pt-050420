8bb9783cb72b42ce18b0f1edc7c14b6dbaf6be14
Catheryne Small <catheryn.small@gmail.com>
2020-05-07 01:31:44 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,7 +1,5 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 def parrot (phrase = "Squawk!")
-  parrot = "Pretty bird!"
-  puts "Pretty bird!"
-  "Pretty bird!"
+  puts "#{phrase}"
 end
